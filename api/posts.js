6f9e7da980ab0f6b3045c842abbe6f28490fcc28
Dev Aryan Dogra <devdogra1@gmail.com>
2023-06-18 23:59:39 +0530

    Set up API routes for Users, Posts

---
+++ api/posts.js
@@ -0,0 +1,30 @@
+const router = require('express').Router(); 
+
+router.get('/', async (req, res, next) => {
+    const posts = await getPostsFromDatabase();
+    res.json(posts);
+})
+
+router.post('/', async (req, res, next) => {
+    const postData = req.body; 
+    const createdPost = await createNewPostInDatabase(postData);
+    res.json(createdPost);
+})
+
+router.patch('/:postId', async (req, res, next) => {
+    const data = req.body; 
+    const updatedPost = await partiallyUpdatePostInDatabase(req.params.postId, data);
+    res.json(updatedPost);
+})
+
+router.put('/:postId', async (req, res, next) => {
+    const data = req.body; 
+    const updatedPost = await updatePostInDatabase(req.params.postId, data); 
+    res.json(updatedPost);
+})
+
+router.delete('/:postId', async (req, res, next) => {
+    const id = req.params.postId; 
+    const deletedPost = await deletePostInDatabase(id);
+    res.json(deletedPost);
+})
---
+++ api/users.js
@@ -0,0 +1,30 @@
+const router = require('express').Router(); 
+
+router.get('/', async (req, res, next) => {
+    const users = await getUsersFromDatabase();
+    res.json(users);
+})
+
+router.post('/', async (req, res, next) => {
+    const userData = req.body; 
+    const createdUser = await createNewUserInDatabase(userData);
+    res.json(createdUser);
+})
+
+router.patch('/:userId', async (req, res, next) => {
+    const data = req.body; 
+    const updatedUser = await partiallyUpdateUserInDatabase(req.params.userId, data);
+    res.json(updatedUser);
+})
+
+router.put('/:userId', async (req, res, next) => {
+    const data = req.body; 
+    const updatedUser = await updateUserInDatabase(req.params.userId, data); 
+    res.json(updatedUser);
+})
+
+router.delete('/:userId', async (req, res, next) => {
+    const id = req.params.userId; 
+    const deletedUser = await deleteUserInDatabase(id);
+    res.json(deletedUser);
+})
