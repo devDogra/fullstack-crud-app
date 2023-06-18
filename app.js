88f188d769f7e05673f4415fe0f582695fb9e303
Dev Aryan Dogra <devdogra1@gmail.com>
2023-06-18 21:36:07 +0530

    Connect mongoose

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express'); 
+const mongoose = require('mongoose');
+
 const port = 8443; 
+const dburi = 'mongodb://127.0.0.1:27017/ocean'
 
 app = express(); 
 
+async function main() {
+
+    await mongoose.connect(dburi);
+    
+    app.listen(port, () => {
+        console.log("Server running at http://localhost:8443");
+    });
+
+}
 
 
-app.listen(port, () => {
-    console.log("Server running at http://localhost:8443");
-})
