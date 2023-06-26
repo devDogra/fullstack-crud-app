79b6edfd7f4fd3705e961e9a2b57b0305c97eadb
Dev Aryan Dogra <devdogra1@gmail.com>
2023-06-26 12:29:00 +0530

    Modify PostSchema to have a title, increase content maxLength to 256

--- models/Post.js
+++ models/Post.js
@@ -3,10 +3,13 @@ const User = require("./User");
 const Schema = mongoose.Schema;
 
 const initialWeight = 1000;
+const maxContentLength = 256;
+const maxTitleLength = 64;
 
 const PostSchema = new Schema({
+  title: { type: String, required: true, maxLength: maxTitleLength },
   author: { type: mongoose.ObjectId, ref: User },
-  content: { type: String, required: true, maxLength: 128 },
+  content: { type: String, required: true, maxLength: maxContentLength },
   weight: { type: Number, default: initialWeight },
 });
 
