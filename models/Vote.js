5fdd234619af86af212291150bfb229eb486bf2e
Dev Aryan Dogra <devdogra1@gmail.com>
2023-06-30 14:24:58 +0530

    Add uniqe together constraint on (post, user) in the VoteSchema

--- models/Vote.js
+++ models/Vote.js
@@ -7,12 +7,17 @@ const UPVOTED = +1;
 const DOWNVOTED = -1;
 const NOTVOTED = 0;
 
+// Not using 'postId' and 'userId' as field names cuz if populate(), fields would sound wrong
 const VoteSchema = new Schema({
     post: { type: mongoose.ObjectId, ref: Post},
     user: { type: mongoose.ObjectId, ref: User},
     value: { type: Number, enum: [DOWNVOTED, NOTVOTED, UPVOTED]}
 })
 
+VoteSchema.index({ post: 1, user: 1 }, { unique: true });
+
+// Since for a user and a post, there can only be case (either liked/disliked or not voted on), [user | post] should be unique together 
+// A vote corresponds to 1 user, voting on 1 post
 
 const Vote = mongoose.model('Vote', VoteSchema);
 module.exports = Vote; 
