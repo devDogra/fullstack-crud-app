0028affec42ecb16e8723cc23db9062f0e88ae0c
Dev Aryan Dogra <devdogra1@gmail.com>
2023-07-10 15:22:25 +0530

    Prevent users from creating Votes in other users' name

--- api/votes.js
+++ api/votes.js
@@ -1,12 +1,43 @@
 const router = require('express').Router(); 
 const mongoose = require('mongoose'); 
+const passport = require("passport"); 
 const Vote = mongoose.model('Vote'); 
 
 console.log("VOTE MODEL");
 console.log(Vote); 
 
+// ensureCanCreateVote => check if trying to create vote's user matches the user aka tryna create his own vote
+
+
+function ensureIsAdmin(req, res, next) {
+    return res.status(403).send({error: "Insufficiant priviliges"})
+}
+
+// And upon success, sets req.user
+function ensureAuthenticated(req, res, next) {
+    passport.authenticate("jwt", { session: false })(req, res, next);
+}
+
+function ensureCanCreateVote(req, res, next) {
+    const user = req.user; // User making the request
+    const vote = req.body;
+    if (user._id.toString() !== vote.user) {
+        // User is trying to create a Vote in another user's name
+        return res.status(401).json({error: "Users cannot create Votes in other users' name"})
+    }
+    console.log({user, vote}); 
+    next();
+}
+
+function ensureCanGetVote(req, res, next) {
+    next();
+}
+function ensureCanUpdateVote(req, res, next) {
+    next();
+}
+
 // EVENTUALLY: We do not want everyone/every usr to see who liked what, right?
-router.get('/', async (req, res, next) => {
+router.get('/', ensureAuthenticated, ensureCanGetVote, async (req, res, next) => {
     try {
         const votes = await Vote.find();
         res.send(votes);
@@ -15,7 +46,7 @@ router.get('/', async (req, res, next) => {
     }
 })
 
-router.get('/:voteId', async (req, res, next) => {
+router.get('/:voteId', ensureAuthenticated, ensureCanGetVote, async (req, res, next) => {
     try {
         const vote = await Vote.findById(req.params.voteId);
         res.send(vote);
@@ -24,7 +55,7 @@ router.get('/:voteId', async (req, res, next) => {
     }
 })
 
-router.post('/', async (req, res, next) => {
+router.post('/', ensureAuthenticated, ensureCanCreateVote, async (req, res, next) => {
     const {user, post, value} = req.body;
     if (!mongoose.isValidObjectId(user) || !mongoose.isValidObjectId(post)) {
         return res.status(404).json({error: "Invalid user or post"})
@@ -44,7 +75,7 @@ router.post('/', async (req, res, next) => {
 })
 
 // Only allow the value of the vote to be updated
-router.put('/:voteId', async (req, res, next) => {
+router.put('/:voteId', ensureAuthenticated, ensureCanUpdateVote, async (req, res, next) => {
     if (!mongoose.isValidObjectId(req.params.voteId)) {
         return res.status(404).json({error: "Invalid ID"});
     }
@@ -64,7 +95,7 @@ router.put('/:voteId', async (req, res, next) => {
     }
 })
 
-router.patch('/:voteId', async (req, res, next) => {
+router.patch('/:voteId', ensureAuthenticated, ensureCanUpdateVote, async (req, res, next) => {
     if (!mongoose.isValidObjectId(req.params.voteId)) {
         return res.status(404).json({error: "Invalid ID"});
     }
